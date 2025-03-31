76433a9eb869ca63616b51266381ac8341387fa3
Bahadir-coder <bahadurmusayev15@gmail.com>
2025-03-31 21:53:40 +0400

    Movie API

--- src/main/java/com/example/internintelligence_movie/service/impl/DirectorServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.internintelligence_movie.service.impl;
-
-import com.example.internintelligence_movie.model.MovieDtoOutput;
-
-import java.util.List;
-
-public interface DirectorServiceImpl {
-    List<MovieDtoOutput> get(String name);
-}
